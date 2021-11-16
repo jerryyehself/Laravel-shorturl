af050b1df2043c933a4a07e7d0f5ecddfbded575
jerryyehself <jerry40522@gmail.com>
2021-11-17 01:24:42 +0800

    cc

--- app/Http/Controllers/Urlcon.php
+++ app/Http/Controllers/Urlcon.php
@@ -46,7 +46,7 @@ class Urlcon extends Controller
 
             
             //$contents = mb_convert_encoding($contents, 'HTML-ENTITIES', "UTF-8");
-            dd($crawler->filterXPath("//body")->text());
+            dd($crawler->filterXPath("//title")->text());
             //$doc -> loadHTML($contents);
             //libxml_use_internal_errors(false);
             
