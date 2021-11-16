b92da5e4858c0cc679c3d62f239b31952669dfd9
jerryyehself <jerry40522@gmail.com>
2021-11-17 01:57:12 +0800

    cc

--- app/Http/Controllers/Urlcon.php
+++ app/Http/Controllers/Urlcon.php
@@ -59,7 +59,7 @@ class Urlcon extends Controller
             //dd(preg_match("/<title>(.*)<\/title>/s", $contents, $match));
 
             $url_title = $crawler -> filterXpath("//title") -> text();
-            dd($url_title);
+            
             $item = new Urltrans;
             $item -> pre_id = $pre_url;
             $item -> new_id = $url_random;
