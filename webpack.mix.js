16b3e9cd417ad087c9845fc83a6a50e66fa67c4c
jerryyehself <jerry40522@gmail.com>
2021-11-17 02:43:57 +0800

    cc

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,3 +15,13 @@ mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
     ]);
+
+mix.webpackConfig({
+    plugins: [
+    ],
+    resolve: {
+    },
+    stats: {
+        children: true
+    }
+});
