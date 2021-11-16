20fa302604f2f6ae76e68f907f80512209110c45
jerryyehself <jerry40522@gmail.com>
2021-11-17 02:29:03 +0800

    cc

---
+++ resources/js/urltrans.js
@@ -0,0 +1,24 @@
+if (document.getElementById("urlt").getAttribute("href") != ""){
+    $(".head-bar").toggleClass("active");
+    $(".title-zh").toggleClass("active");
+    $(".title-eng").toggleClass("active");
+    $(".input-mag").toggleClass("active");
+    $(".function-input").toggleClass("active");
+    $(".url").toggleClass("active");
+    $(".main-content").toggleClass("active");
+    $(".footer").toggleClass("active");
+    $(".title-font").toggleClass("active");
+    $(".input-msg").remove();
+    $(function() {
+            var arr = $('.url-tran').find('input').toArray();// 把三個div放進數組裡面
+            var temp;
+            // 1 3對調
+            temp = arr[0];
+            arr[0] = arr[1];
+            arr[1] = temp;
+         
+            $('.url-tran').html(arr);
+        })
+}
+
+
