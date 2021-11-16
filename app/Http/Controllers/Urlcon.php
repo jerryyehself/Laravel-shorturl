febe705e9f9aee139d05399719274f10186f94aa
jerryyehself <jerry40522@gmail.com>
2021-11-16 18:28:36 +0800

    cc

--- app/Http/Controllers/Urlcon.php
+++ app/Http/Controllers/Urlcon.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Urltrans;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\DB;
 
 class Urlcon extends Controller
