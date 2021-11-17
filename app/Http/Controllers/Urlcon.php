65085d02ee090aa8177e098204d4fff469b8db9f
jerryyehself <jerry40522@gmail.com>
2021-11-17 14:14:48 +0800

    cc

--- app/Http/Controllers/Urlcon.php
+++ app/Http/Controllers/Urlcon.php
@@ -77,9 +77,10 @@ class Urlcon extends Controller
             $item -> new_id = $sql -> new_id;
             $item -> url_title = $sql -> url_title;
             $item -> number_of_inseret_times = $sql -> number_of_inseret_times;
-            $item -> increment('number_of_inseret_times') ->where('pre_id', $pre_url) -> first() -> save();
+            $item -> increment('number_of_inseret_times') -> save();
             //$new_insert_number = $item -> number_of_inseret_times;
             //$item -> number_of_inseret_times = $new_insert_number++;
+            
 
             return view('/welcome', ['pre_url'=> $pre_url, 'new_id'=> $item -> new_id, 'url_title'=> $item -> url_title ]);
 
