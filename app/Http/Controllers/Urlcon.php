41628c088081ba8b86a206c5ba470b2ed6f91139
jerryyehself <jerry40522@gmail.com>
2021-11-21 03:37:38 +0800

    cc

--- app/Http/Controllers/Urlcon.php
+++ app/Http/Controllers/Urlcon.php
@@ -89,7 +89,7 @@ class Urlcon extends Controller
             
             
             //dd($item -> uasge_number);*/
-            $new_insert_number = DB::table('urltrans')-> increment('number_of_inseret_times', 1, ['pre_id' => $pre_url]);
+        $new_insert_number = DB::table('urltrans')-> where('pre_id', $pre_url)  -> increment('number_of_inseret_times'/*, 1, ['pre_id' => $pre_url]*/);
 
             //dd($sql);
 
