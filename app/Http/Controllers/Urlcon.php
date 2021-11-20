337b5d499bce9565105381a54a44f451e1352ed4
jerryyehself <jerry40522@gmail.com>
2021-11-21 01:29:06 +0800

    cc

--- app/Http/Controllers/Urlcon.php
+++ app/Http/Controllers/Urlcon.php
@@ -77,7 +77,7 @@ class Urlcon extends Controller
             //$item -> url_update_time =  $sql -> url_update_time;
             $item -> url_host = $sql -> url_host;
             $item -> uasge_number = $sql -> usage_number;
-
+            dd($item -> usage_number);
             $new_insert_number = DB::table('urltrans')-> increment('number_of_inseret_times', 1, ['pre_id' => $pre_url]);
 
             return view('/welcome', ['pre_url'=> $pre_url,
