4a54e6b483f9b80b754777b3a2913c3dcd1a7bf5
jerryyehself <jerry40522@gmail.com>
2021-11-21 02:47:26 +0800

    cc

--- app/Http/Controllers/Urlcon.php
+++ app/Http/Controllers/Urlcon.php
@@ -67,7 +67,7 @@ class Urlcon extends Controller
                                      'new_id'=>$item -> new_id,
                                      'url_title'=> $url_title,
                                      'ins_time'=>$item -> ins_time,
-                                     //'url_update_time'=>$item -> url_update_time,
+                                     'url_update_time'=>$item -> url_update_time,
                                      'number_of_inseret_times'=> $item -> number_of_inseret_times,
                                      'url_host' => $url_host,
                                      'usage_number' => $item -> usage_number]);               
@@ -96,7 +96,7 @@ class Urlcon extends Controller
                                      'new_id'=> $sql -> new_id,
                                      'url_title'=> $sql -> url_title,
                                      'ins_time'=> $sql -> ins_time,
-                                     //'url_update_time'=>$item -> url_update_time,
+                                     'url_update_time'=>$item -> url_update_time,
                                      'number_of_inseret_times'=> $sql -> number_of_inseret_times,
                                      'url_host' => $sql -> url_host,
                                      'usage_number' => $sql -> uasge_number]);
