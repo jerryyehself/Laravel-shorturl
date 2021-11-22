30c336f439e58e6f015f1740d1a5f95156e4a229
jerryyehself <jerry40522@gmail.com>
2021-11-23 03:42:26 +0800

    cc

--- app/Http/Controllers/Urlcon.php
+++ app/Http/Controllers/Urlcon.php
@@ -29,7 +29,7 @@ class Urlcon extends Controller
                                 'url_update_time'=> '']);
         } 
 
-        date_default_timezone_set("Asia/Taipei");
+        //date_default_timezone_set("Asia/Taipei");
         
         $sql = DB::table('urltrans')->where('pre_id', $pre_url)->first();
         
@@ -84,7 +84,7 @@ class Urlcon extends Controller
             return view('/welcome', ['pre_url'=> $pre_url,
                                      'new_id'=> $sql -> new_id,
                                      'url_title'=> $sql -> url_title,
-                                     'ins_time'=> $sql -> ins_time,
+                                     'ins_time'=> $sql -> ins_time ->format('y-m-d h:i:s'),
                                      //'url_update_time'=> $new_update_time,
                                      'number_of_inseret_times'=> $sql -> number_of_inseret_times,
                                      'url_host' => $sql -> url_host,
