ddb87d423da0adfcf340ad571b071297c9e5957a
jerryyehself <jerry40522@gmail.com>
2021-11-22 13:01:34 +0800

    cc

--- app/Http/Controllers/Urlcon.php
+++ app/Http/Controllers/Urlcon.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Symfony\Component\DomCrawler\Crawler;
+use Carbon\Carbon;
 
 class Urlcon extends Controller
 {
