6e7dce06d5173c1667a9a00c9a5f72ce8ef5ab0a
jerryyehself <jerry40522@gmail.com>
2021-11-22 12:55:01 +0800

    cc

--- app/Http/Controllers/Urlcon.php
+++ app/Http/Controllers/Urlcon.php
@@ -83,7 +83,7 @@ class Urlcon extends Controller
             return view('/welcome', ['pre_url'=> $pre_url,
                                      'new_id'=> $sql -> new_id,
                                      'url_title'=> $sql -> url_title,
-                                     'ins_time'=> $sql -> ins_time -> format('d-m-Y h:i:s'),
+                                     'ins_time'=> $sql -> ins_time,
                                      //'url_update_time'=> $new_update_time,
                                      'number_of_inseret_times'=> $sql -> number_of_inseret_times,
                                      'url_host' => $sql -> url_host,
