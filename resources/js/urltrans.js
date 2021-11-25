46af0bf98291b73066edb4fb98a8441db58d39b5
jerryyehself <jerry40522@gmail.com>
2021-11-25 21:33:00 +0800

    cc

--- resources/js/urltrans.js
+++ resources/js/urltrans.js
@@ -1,42 +1,36 @@
-
+if (document.getElementById("url-string").getAttribute("href") != ""){
+    $(".head-bar").toggleClass("active");
+    $(".title-zh").toggleClass("active");
+    $(".title-eng").toggleClass("active");
+    $(".input-mag").toggleClass("active");
+    $(".function-input").toggleClass("active");
+    $(".url").toggleClass("active");
+    $(".main-content").toggleClass("active");
+    $(".footer").toggleClass("active");
+    $(".title-font").toggleClass("active");
+    $(".input-msg").remove();
+    $(function() {
+            var arr = $('.url-tran').find('input').toArray();// 把三個div放進數組裡面
+            var temp;
+            // 1 3對調
+            temp = arr[0];
+            arr[0] = arr[1];
+            arr[1] = temp;
+         
+            $('.url-tran').html(arr);
+        });
+    $(".submit").css("display", "none");
+};
 
 
 $(document).ready(function(){
 
-    if (document.getElementById("url-string").getAttribute("href") != ""){
-        $(".head-bar").toggleClass("active");
-        $(".title-zh").toggleClass("active");
-        $(".title-eng").toggleClass("active");
-        $(".input-mag").toggleClass("active");
-        $(".function-input").toggleClass("active");
-        $(".url").toggleClass("active");
-        $(".main-content").toggleClass("active");
-        $(".footer").toggleClass("active");
-        $(".title-font").toggleClass("active");
-        $(".input-msg").remove();
-        $(function() {
-                var arr = $('.url-tran').find('input').toArray();// 把三個div放進數組裡面
-                var temp;
-                // 1 3對調
-                temp = arr[0];
-                arr[0] = arr[1];
-                arr[1] = temp;
-             
-                $('.url-tran').html(arr);
-            });
+    
+    $(".url.active").on('focus', function(event){
+        $(".submit").css("display", "inline-block");
+    });    
+        
+    $(".url.active").on('blur', function(event){
         $(".submit").css("display", "none");
-
-        $(".url.active").on('focus', function(event){
-            $(".submit").css("display", "inline-block");
-        });    
-        
-        $(".url.active").on('blur', function(event){
-            $(".submit").css("display", "none");
-        });
-        
-    };
-
-
+    });   
 });
-
-
