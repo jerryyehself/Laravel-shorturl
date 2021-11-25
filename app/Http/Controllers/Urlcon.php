3a84bbd31b69d20e09fc852b0118dfefafbce81c
jerryyehself <jerry40522@gmail.com>
2021-11-26 02:40:06 +0800

    cc

--- app/Http/Controllers/Urlcon.php
+++ app/Http/Controllers/Urlcon.php
@@ -48,7 +48,7 @@ class Urlcon extends Controller
 
             $crawler -> addHtmlContent($html_content);
 
-            dd($crawler -> document);
+            dd($crawler -> addHtmlContent($html_content));
             
             $url_title = $crawler -> filterXpath("//title") -> text();
 
