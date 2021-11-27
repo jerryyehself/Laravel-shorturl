f210299eb1d1d8f5d3a0b2fa7a75878b1bb69cb8
jerryyehself <jerry40522@gmail.com>
2021-11-27 23:02:38 +0800

    cc

--- resources/js/urltrans.js
+++ resources/js/urltrans.js
@@ -74,11 +74,14 @@ var dataset = [ 5, 10, 15, 20, 25 ];
         return barHeight + "px";
     });*/
 
-svg.selectAll("rect")
+    svg.selectAll("rect")
     .data(dataset)
     .enter()
     .append("rect")
-    .attr("x", 0)
+    .attr("x", function(d, i) {
+            return i * 21;	//Bar width of 20 plus 1 for padding
+    })
     .attr("y", 0)
     .attr("width", 20)
     .attr("height", 100);
+ 
