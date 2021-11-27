71c2a88428acd2a2ce168ea5c72e389694a48947
jerryyehself <jerry40522@gmail.com>
2021-11-28 02:34:17 +0800

    cc

--- public/js/app.js
+++ public/js/app.js
@@ -54,17 +54,17 @@ $(document).ready(function () {
     $(".submit").css("display", "none");
   });
   var dataset = [5, 10, 13, 19, 21, 25, 22, 18, 15, 13, 11, 12, 15, 20, 18, 17, 16, 18, 23, 25];
-  var w = 2000;
-  var h = 2000;
-  var barPadding = 10;
+  var w = 500;
+  var h = 100;
+  var barPadding = 1;
   var svg = d3__WEBPACK_IMPORTED_MODULE_0__.select(".chart-output").append("svg");
-  svg.selectAll("rect").data(dataset).enter().append("rect").attr("x", function (d, i) {
+  svg.selectAll("rect").data(dataset).enter().append("rect").attr("height", function (d) {
+    return d * 4;
+  }).attr("x", function (d, i) {
     return i * (w / dataset.length);
   }).attr("y", function (d) {
     return h - d * 4;
-  }).attr("width", w / dataset.length - barPadding).attr("height", function (d) {
-    return d * 4;
-  });
+  }).attr("width", w / dataset.length - barPadding);
 });
 
 /***/ }),
--- resources/js/urltrans.js
+++ resources/js/urltrans.js
@@ -37,9 +37,9 @@ $(document).ready(function(){
     var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
         11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ];
 
-    var w = 2000;
-    var h = 2000;
-    var barPadding = 10;
+    var w = 500;
+    var h = 100;
+    var barPadding = 1;
 
     var svg = d3.select(".chart-output")
                 .append("svg")
@@ -49,6 +49,9 @@ $(document).ready(function(){
             .data(dataset)
             .enter()
             .append("rect")
+            .attr("height", function(d) {
+                return d * 4;
+            })
             .attr("x", function(d, i) {
                     return i * (w / dataset.length);
             })
@@ -56,8 +59,6 @@ $(document).ready(function(){
                     return h - (d * 4);
             })
             .attr("width", w / dataset.length - barPadding)
-            .attr("height", function(d) {
-                    return d * 4;
-            });
+
 });
 
