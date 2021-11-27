271c31faa99415a2b4b751494a3173805392539f
jerryyehself <jerry40522@gmail.com>
2021-11-28 02:25:08 +0800

    cc

--- public/js/app.js
+++ public/js/app.js
@@ -53,9 +53,9 @@ $(document).ready(function () {
   $(".url.active").on('blur', function (event) {
     $(".submit").css("display", "none");
   });
-  var dataset = [5, 10, 13, 19, 21, 25, 22, 18, 15, 13, 11, 12, 15, 20, 18, 17, 16, 18, 23, 25]; //var w = 2000;
-  //var h = 2000;
-
+  var dataset = [5, 10, 13, 19, 21, 25, 22, 18, 15, 13, 11, 12, 15, 20, 18, 17, 16, 18, 23, 25];
+  var w = 2000;
+  var h = 2000;
   var barPadding = 1;
   var svg = d3__WEBPACK_IMPORTED_MODULE_0__.select(".chart-output").append("svg");
   svg.selectAll("rect").data(dataset).enter().append("rect").attr("x", function (d, i) {
