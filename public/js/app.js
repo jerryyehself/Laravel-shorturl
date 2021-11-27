68bba89e75ae181b76995ae9816c6fbbaa50c1b7
jerryyehself <jerry40522@gmail.com>
2021-11-28 00:32:51 +0800

    cc

--- public/js/app.js
+++ public/js/app.js
@@ -53,10 +53,10 @@ $(document).ready(function () {
   $(".url.active").on('blur', function (event) {
     $(".submit").css("display", "none");
   });
-  var dataset = [5, 10, 13, 19, 21, 25, 22, 18, 15, 13, 11, 12, 15, 20, 18, 17, 16, 18, 23, 25];
-  var w = "100%";
-  var h = "100%";
-  var svg = d3__WEBPACK_IMPORTED_MODULE_0__.select(".chart-output").append("svg").attr("width", w).attr("height", h);
+  var dataset = [5, 10, 13, 19, 21, 25, 22, 18, 15, 13, 11, 12, 15, 20, 18, 17, 16, 18, 23, 25]; //var w = 100%;
+  //var h = 100;
+
+  var svg = d3__WEBPACK_IMPORTED_MODULE_0__.select(".chart-output").append("svg").attr("width", "100%").attr("height", "100%");
   svg.selectAll("rect").data(dataset).enter().append("rect").attr("y", 0).attr("width", 20).attr("height", 100).attr("x", function (d, i) {
     return i * (w / dataset.length);
   });
