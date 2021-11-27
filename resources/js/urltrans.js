bddc30e3bafa160ebb13867b31e3c99776d56141
jerryyehself <jerry40522@gmail.com>
2021-11-28 00:34:45 +0800

    cc

--- resources/js/urltrans.js
+++ resources/js/urltrans.js
@@ -37,13 +37,13 @@ $(document).ready(function(){
     var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
         11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ];
 
-    //var w = 100%;
-    //var h = 100;
+    var w = "100%";
+    var h = "100%";
 
     var svg = d3.select(".chart-output")
                 .append("svg")
-                .attr("width", "100%")
-                .attr("height", "100%");
+                .attr("width", w)
+                .attr("height", h);
 
     svg.selectAll("rect")
         .data(dataset)
