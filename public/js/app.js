b9a7cda5fb715dc8fa12f7db14ef950c032013a0
jerryyehself <jerry40522@gmail.com>
2021-11-28 02:42:56 +0800

    cc

--- public/js/app.js
+++ public/js/app.js
@@ -54,7 +54,7 @@ $(document).ready(function () {
     $(".submit").css("display", "none");
   });
   var dataset = [5, 10, 13, 19, 21, 25, 22, 18, 15, 13, 11, 12, 15, 20, 18, 17, 16, 18, 23, 25];
-  var w = 500;
+  var w = 5000;
   var h = 100;
   var barPadding = 1;
   var svg = d3__WEBPACK_IMPORTED_MODULE_0__.select(".chart-output").append("svg");
