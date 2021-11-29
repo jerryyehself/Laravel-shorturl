0bd84df46ae4f9cacd9c5feb00f51b573215dbd7
jerryyehself <jerry40522@gmail.com>
2021-11-29 18:24:12 +0800

    cc

--- resources/js/urltrans.js
+++ resources/js/urltrans.js
@@ -22,6 +22,9 @@ if (document.getElementById("url-string").getAttribute("href") != ""){
     $(".submit").css("display", "none");
 };
 
+var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
+    11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ];
+
 import * as d3 from "d3";
 $(document).ready(function(){
     
@@ -33,8 +36,7 @@ $(document).ready(function(){
         $(".submit").css("display", "none");
     });
         
-    var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
-        11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ];
+
 
     var svg = d3.select(".chart-output")
                 .append("svg")
