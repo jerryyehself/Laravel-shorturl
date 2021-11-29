56e0776fe1f0961b2a2bd10e3082c70f82571831
jerryyehself <jerry40522@gmail.com>
2021-11-29 17:11:04 +0800

    cc

--- public/js/app.js
+++ public/js/app.js
@@ -54,13 +54,17 @@ $(document).ready(function () {
     $(".submit").css("display", "none");
   });
   var dataset = [5, 10, 13, 19, 21, 25, 22, 18, 15, 13, 11, 12, 15, 20, 18, 17, 16, 18, 23, 25];
-  var w = 500;
-  var h = 100;
+  var svg = d3__WEBPACK_IMPORTED_MODULE_0__.select(".chart-output").append("svg").attr("id", "chart");
+  var w = document.getElementById("chart").clientWidth;
+  var h = document.getElementById("chart").clientHeight;
   var barPadding = 1;
-  var svg = d3__WEBPACK_IMPORTED_MODULE_0__.select(".chart-output").select("svg").append("svg").attr("id", "chart");
-  svg.selectAll("rect").data(dataset).enter().append("rect").attr('height', function (d, i) {
-    return 10000 - yScale(d);
-  }).attr("width", w / dataset.length - barPadding);
+  svg.selectAll("rect").data(dataset).enter().append("rect").attr("x", function (d, i) {
+    return i * (w / dataset.length);
+  }).attr("y", function (d) {
+    return h - d * 4;
+  }).attr("width", w / dataset.length - barPadding).attr("height", function (d) {
+    return d * 4;
+  });
 });
 
 /***/ }),
--- resources/js/urltrans.js
+++ resources/js/urltrans.js
@@ -37,22 +37,30 @@ $(document).ready(function(){
     var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
         11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ];
 
-    var w = 500;
-    var h = 100;
-    var barPadding = 1;
+
 
     var svg = d3.select(".chart-output")
-                .select("svg")
                 .append("svg")
-                .attr("id", "chart");
+                .attr("id", "chart")
+    
+    var w = document.getElementById("chart").clientWidth;
+    var h = document.getElementById("chart").clientHeight;
+    var barPadding = 1;
 
-        svg.selectAll("rect")
-            .data(dataset)
-            .enter()
-            .append("rect")
-            .attr('height', (d, i) => 10000 - yScale(d))
-            .attr("width", w / dataset.length - barPadding)
-
+    svg.selectAll("rect")
+			   .data(dataset)
+			   .enter()
+			   .append("rect")
+			   .attr("x", function(d, i) {
+			   		return i * (w / dataset.length);
+			   })
+			   .attr("y", function(d) {
+			   		return h - (d * 4);
+			   })
+			   .attr("width", w / dataset.length - barPadding)
+			   .attr("height", function(d) {
+			   		return d * 4;
+			   });
 
 });
 
