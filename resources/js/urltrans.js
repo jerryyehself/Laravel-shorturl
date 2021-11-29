f8233e76eccd6326c4a36b40229305938560830b
jerryyehself <jerry40522@gmail.com>
2021-11-29 18:28:01 +0800

    cc

--- resources/js/urltrans.js
+++ resources/js/urltrans.js
@@ -22,15 +22,6 @@ if (document.getElementById("url-string").getAttribute("href") != ""){
     $(".submit").css("display", "none");
 };
 
-
-var svg = d3.select(".chart-output")
-.append("svg")
-.attr("id", "canvas");
-
-
-var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
-    11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ];
-
 import * as d3 from "d3";
 $(document).ready(function(){
     
@@ -42,8 +33,13 @@ $(document).ready(function(){
         $(".submit").css("display", "none");
     });
         
+    var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
+        11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ];
 
-
+    var svg = d3.select(".chart-output")
+                .append("svg")
+                .attr("id", "canvas");
+    
     var w = document.getElementById("canvas").clientWidth;
     var h = document.getElementById("canvas").clientHeight;
     var barPadding = 1;
@@ -52,6 +48,7 @@ $(document).ready(function(){
         .data(dataset)
         .enter()
         .append("rect")
+        .attr("id", "canvas")
         .attr("x", function(d, i) {
                 return i * (w / dataset.length);
         })
