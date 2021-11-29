63ffcbb6d9cfcd7139c7e6499ff0b7a1f275528f
jerryyehself <jerry40522@gmail.com>
2021-11-29 18:24:53 +0800

    cc

--- public/js/app.js
+++ public/js/app.js
@@ -45,6 +45,7 @@ if (document.getElementById("url-string").getAttribute("href") != "") {
 }
 
 ;
+var dataset = [5, 10, 13, 19, 21, 25, 22, 18, 15, 13, 11, 12, 15, 20, 18, 17, 16, 18, 23, 25];
 
 $(document).ready(function () {
   $(".url.active").on('focus', function (event) {
@@ -53,7 +54,6 @@ $(document).ready(function () {
   $(".url.active").on('blur', function (event) {
     $(".submit").css("display", "none");
   });
-  var dataset = [5, 10, 13, 19, 21, 25, 22, 18, 15, 13, 11, 12, 15, 20, 18, 17, 16, 18, 23, 25];
   var svg = d3__WEBPACK_IMPORTED_MODULE_0__.select(".chart-output").append("svg").attr("id", "canvas");
   var w = document.getElementById("canvas").clientWidth;
   var h = document.getElementById("canvas").clientHeight;
