98a3c97269f9b3ae0163a285d6c05179c83b3fee
jerryyehself <jerry40522@gmail.com>
2021-12-01 03:33:27 +0800

    cc

--- resources/js/urltrans.js
+++ resources/js/urltrans.js
@@ -33,8 +33,8 @@ $(document).ready(function(){
         $(".submit").css("display", "none");
     });
 
-    var usage_num = "<?php echo $sql -> usage_number;?>" ;
-    var trans_num = "<?php echo $sql -> number_of_inseret_times;?>";
+    var usage_num = "<?=$sql -> usage_number?>" ;
+    var trans_num = "<?=$sql -> number_of_inseret_times?>";
         
     var dataset = [usage_num, trans_num];
     
