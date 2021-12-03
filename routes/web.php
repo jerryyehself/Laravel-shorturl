57430657d3f624747997f981ea8b402375344795
jerryyehself <jerry40522@gmail.com>
2021-12-04 00:34:02 +0800

    cc

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use App\Http\Controllers\Urlcon;
 
 Route::get('/', [Urlcon::class, 'turn_in']);
 
-Route::get('/show_chart',);
+Route::get('/show_chart',function(){return view("/show_chart");});
 
 Route::get('/{codee}', [Urlcon::class, 'turn_t']);
