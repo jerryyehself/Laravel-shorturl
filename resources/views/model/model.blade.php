1e2bf1e449485a77d3562f48fec15e27581850b6
jerryyehself <jerry40522@gmail.com>
2021-12-06 19:32:23 +0800

    cc

--- resources/views/model/model.blade.php
+++ resources/views/model/model.blade.php
@@ -4,8 +4,8 @@
         <link rel="icon" href="{{asset('imgs/short_images.jpeg')}}" type="image/x-icon">
         <link rel="stylesheet" type="text/css" href="css/app.css">
         <script src="{{asset('js/jquery.min.js')}}" defer></script>
-        <!--<script src="{{asset('js/app.js')}}" defer></script>-->
-        <script src="{{asset('js/show_chart.js')}}" defer></script>
+        <script src="{{asset('js/app.js')}}" defer></script>
+        <!--script src="{{asset('js/show_chart.js')}}" defer></script>-->
         <script src="https://d3js.org/d3.v7.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     </head>
