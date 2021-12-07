51adf7cb4068e6b66a72c12b0ca83c7eb6c13327
jerryyehself <jerry40522@gmail.com>
2021-12-08 02:02:50 +0800

    cc

--- resources/js/urltrans.js
+++ resources/js/urltrans.js
@@ -13,7 +13,6 @@ if (document.getElementById("url-string").getAttribute("href") !== ""){
     $(".chart-output").toggleClass("active");
     //$("input").toggleClass("active");
     //$("div").toggleClass("active");
-    
     //$("footer").toggleClass("active");
     $(".input-msg").remove();
     $(function() {
