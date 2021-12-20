86cbb3732134024d1b5eb960e66fdfb8b15bcacf
jerryyehself <jerry40522@gmail.com>
2021-12-21 04:39:04 +0800

    cc

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,7 +25,7 @@
         <div class="chartlist">
             
             <div class="chartType">@{{ chartList }}</div>
-            <div class="list-content" v-for="chartList in chartLists" onclick='ttry()'>
+            <div class="list-content" v-for="chartList in chartLists" onclick='window.ttry()'>
                 @{{ chartList }}
             </div>
 
