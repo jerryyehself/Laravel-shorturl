1bfa1af6323048cbc29d93c828008606dff08edc
jerryyehself <jerry40522@gmail.com>
2021-12-21 01:59:08 +0800

    cc

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,7 +24,7 @@
 
         <div class="chartlist">
             
-            <div class="list-content" v-for="chartList in chartLists" @onclick="boxShadow = false">
+            <div class="list-content" v-for="chartList in chartLists" @click="boxShadow = false">
                 @{{ chartList }}
             </div>
 
