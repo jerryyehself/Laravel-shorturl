8e85694ddc8984ebc398c1731bf9fcc74339fc6f
jerryyehself <jerry40522@gmail.com>
2021-12-22 02:23:07 +0800

    cc

--- resources/js/urltrans.js
+++ resources/js/urltrans.js
@@ -129,14 +129,14 @@ $(document).ready(function(){
 
                 this.defultChart = chartType;
 
-                if(this.defultChart === 'chart.js'){
+                    if(this.defultChart === 'chart.js'){
 
-                    showChartjs(urlData);
+                        showChartjs(urlData);
 
-                }else if(this.defultChart === 'd3.js'){
+                    }else if(this.defultChart === 'd3.js'){
 
-                    showD3js(urlData);
-                }
+                        showD3js(urlData);
+                    }
                 return this.chartType;
             }
         }
