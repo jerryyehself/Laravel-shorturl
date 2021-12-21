8cc3feacfbcfe69addb62c0df089a3e90286e3d6
jerryyehself <jerry40522@gmail.com>
2021-12-21 22:21:05 +0800

    cc

--- public/js/app.js
+++ public/js/app.js
@@ -17177,89 +17177,72 @@ var _require = __webpack_require__(/*! vue */ "./node_modules/vue/index.js"),
     TrackOpTypes = _require.TrackOpTypes;
 
 
-/*function showChartjs(){
 
-        const labels = [
-            '2013',
-            '2014',
-            '2015',
-            '2016',
-            '2017',
-            '2018',
-            '2019'
-        ];
-        
-        const suicide = {
-            type:'line',
-            label: '全台自殺人數',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [5285, 5554, 5842, 5592, 5723, 5901, 7103],
-        };
+function showChartjs() {
+  var labels = ['2013', '2014', '2015', '2016', '2017', '2018', '2019'];
+  var suicide = {
+    type: 'line',
+    label: '全台自殺人數',
+    backgroundColor: 'rgb(255, 99, 132)',
+    borderColor: 'rgb(255, 99, 132)',
+    data: [5285, 5554, 5842, 5592, 5723, 5901, 7103]
+  };
+  var antidepressant = {
+    type: 'line',
+    label: '全台抗憂鬱藥物使用人數',
+    backgroundColor: 'rgb(255, 255, 132)',
+    borderColor: 'rgb(60, 95, 189)',
+    data: [1141151, 1165942, 1194395, 1212659, 1273561, 1330204, 1397197]
+  };
+  var config = {
+    type: 'scatter',
+    data: {
+      labels: labels,
+      datasets: [suicide, antidepressant]
+    },
+    options: {}
+  };
+  var myChart = new Chart(document.$('#cjs'), config);
+}
 
-        const antidepressant = {
-            type:'line',
-            label: '全台抗憂鬱藥物使用人數',
-            backgroundColor: 'rgb(255, 255, 132)',
-            borderColor: 'rgb(60, 95, 189)',
-            data: [1141151, 1165942, 1194395, 1212659, 1273561, 1330204, 1397197],
-        };
-
-        const config = {
-            type: 'scatter',
-            data: {labels: labels,datasets:[suicide, antidepressant]},
-            options:{}
-        };
-
-        const myChart = new Chart(
-            document.$('#cjs'),
-            config
-        )};
-
-    function showD3js(dataset){
-
-        var svg = d3.select("#djs")
-                    .append("svg")
-        
-        $("svg").attr("id", "canvas");
-
-        const w = document.getElementById("canvas").clientWidth;
-        const h = document.getElementById("canvas").clientHeight;
-        const barPadding = 1;
-
-        const yScale = d3.scaleLinear() //製作線性尺度
-                        .domain([0, 100]) //輸入的範圍
-                        .range([h - barPadding, barPadding])                         
-
-        const yAxis = d3.axisLeft(yScale)
-                        .ticks(10)
-
-        svg.selectAll("rect")
-            .data(dataset)
-            .enter()
-            .append("rect")
-            .attr("x", function(d, i) {return i * ((w-20) / dataset.length) + 20;})
-            .attr("y", function(d) {return h - (d * 4);})
-            .attr("width", (w  - (20*(dataset.length+barPadding)) - barPadding) / dataset.length+20 - barPadding)
-            .attr("height", function(d) {return d * 4;})
-            .attr("fill", function(d) {return "rgb(0, 0, " + (d * 10) + ")";});
-
-        svg.selectAll("text")
-            .data(dataset)
-            .enter()
-            .append("text")
-            .text(function(d) {return d;})
-            .attr("text-anchor", "middle")
-            .attr("x", function(d, i) {return i * (w / dataset.length  - barPadding) + (w / dataset.length - barPadding) / 2 + 20;})
-            .attr("y", function(d) {return h - (d * 4) + 14;})
-            .attr("font-family", "sans-serif")
-            .attr("font-size", "11px")
-            .attr("fill", "white");
-
-        svg.append('g').attr('class', 'axis')
-            .attr('transform', 'translate(20)', 0) //移動到左方
-            .call(yAxis);
-    }*/
+;
+/*function showD3js(dataset){
+      var svg = d3.select("#djs")
+                .append("svg")
+    
+    $("svg").attr("id", "canvas");
+      const w = document.getElementById("canvas").clientWidth;
+    const h = document.getElementById("canvas").clientHeight;
+    const barPadding = 1;
+      const yScale = d3.scaleLinear() //製作線性尺度
+                    .domain([0, 100]) //輸入的範圍
+                    .range([h - barPadding, barPadding])                         
+      const yAxis = d3.axisLeft(yScale)
+                    .ticks(10)
+      svg.selectAll("rect")
+        .data(dataset)
+        .enter()
+        .append("rect")
+        .attr("x", function(d, i) {return i * ((w-20) / dataset.length) + 20;})
+        .attr("y", function(d) {return h - (d * 4);})
+        .attr("width", (w  - (20*(dataset.length+barPadding)) - barPadding) / dataset.length+20 - barPadding)
+        .attr("height", function(d) {return d * 4;})
+        .attr("fill", function(d) {return "rgb(0, 0, " + (d * 10) + ")";});
+      svg.selectAll("text")
+        .data(dataset)
+        .enter()
+        .append("text")
+        .text(function(d) {return d;})
+        .attr("text-anchor", "middle")
+        .attr("x", function(d, i) {return i * (w / dataset.length  - barPadding) + (w / dataset.length - barPadding) / 2 + 20;})
+        .attr("y", function(d) {return h - (d * 4) + 14;})
+        .attr("font-family", "sans-serif")
+        .attr("font-size", "11px")
+        .attr("fill", "white");
+      svg.append('g').attr('class', 'axis')
+        .attr('transform', 'translate(20)', 0) //移動到左方
+        .call(yAxis);
+}*/
 
 if (document.getElementById("url-string").getAttribute("href") !== "") {
   $(".head-bar").toggleClass("active");
--- resources/js/urltrans.js
+++ resources/js/urltrans.js
@@ -1,46 +1,46 @@
 const { TrackOpTypes } = require("vue");
 import * as d3 from "d3";
 
-/*function showChartjs(){
+function showChartjs(){
 
-        const labels = [
-            '2013',
-            '2014',
-            '2015',
-            '2016',
-            '2017',
-            '2018',
-            '2019'
-        ];
+    const labels = [
+        '2013',
+        '2014',
+        '2015',
+        '2016',
+        '2017',
+        '2018',
+        '2019'
+    ];
         
-        const suicide = {
-            type:'line',
-            label: '全台自殺人數',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [5285, 5554, 5842, 5592, 5723, 5901, 7103],
-        };
+    const suicide = {
+        type:'line',
+        label: '全台自殺人數',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [5285, 5554, 5842, 5592, 5723, 5901, 7103],
+    };
 
-        const antidepressant = {
-            type:'line',
-            label: '全台抗憂鬱藥物使用人數',
-            backgroundColor: 'rgb(255, 255, 132)',
-            borderColor: 'rgb(60, 95, 189)',
-            data: [1141151, 1165942, 1194395, 1212659, 1273561, 1330204, 1397197],
-        };
+    const antidepressant = {
+        type:'line',
+        label: '全台抗憂鬱藥物使用人數',
+        backgroundColor: 'rgb(255, 255, 132)',
+        borderColor: 'rgb(60, 95, 189)',
+        data: [1141151, 1165942, 1194395, 1212659, 1273561, 1330204, 1397197],
+    };
 
-        const config = {
-            type: 'scatter',
-            data: {labels: labels,datasets:[suicide, antidepressant]},
-            options:{}
-        };
+    const config = {
+        type: 'scatter',
+        data: {labels: labels,datasets:[suicide, antidepressant]},
+        options:{}
+    };
 
-        const myChart = new Chart(
-            document.$('#cjs'),
-            config
-        )};
+    const myChart = new Chart(
+        document.$('#cjs'),
+        config
+    )};
 
-    function showD3js(dataset){
+    /*function showD3js(dataset){
 
         var svg = d3.select("#djs")
                     .append("svg")
